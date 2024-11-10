d9962d4a27e4f7c601e0b271957df303d84b0b03
Yael Koppel <yaelkoppel1@gmail.com>
2024-11-10 13:08:16 +0200

    commit

--- src/app/api/recipes/route.tsx
+++ src/app/api/recipes/route.tsx
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { getAllDocuments,connectDatabase,insertDocument,deleteDocument } from "@/services/mongo";
+import { getAllDocuments,connectDatabase,insertDocument,deleteDocument, getDatabaseClient } from "@/services/mongo";
 
 
 export async function GET(request: Request)
@@ -12,7 +12,7 @@ export async function GET(request: Request)
 }
 
 export async function POST(request: Request) {
-    const client = await connectDatabase();
+    const client = await getDatabaseClient();
   
     try {
       const newRecipe = await request.json();
@@ -21,19 +21,21 @@ export async function POST(request: Request) {
       client.close();
       return NextResponse.json({ data: { ...newRecipe, _id: insertedRecipeId } });
     } catch (error) {
-      client.close();
       return NextResponse.json({ error: 'Failed to add document' }, { status: 500 });
     }
   }
 
 export async function DELETE(request: Request) {
     const { searchParams } = new URL(request.url);
-    const id = searchParams.get('id');
-    if (!id) return NextResponse.json({ message: 'Recipe ID not provided' }, { status: 400 });
-  
-    const client = await connectDatabase();
+    const idParam = searchParams.get('id');
+    if (!idParam) return NextResponse.json({ message: 'Recipe ID not provided' }, { status: 400 });
+
+    const id = parseInt(idParam, 10);
+    if (isNaN(id)) {
+        return NextResponse.json({ message: 'Recipe ID must be a valid number' }, { status: 400 });
+    }
+    const client = await getDatabaseClient();
     const result = await deleteDocument(client, 'recipes', id);
-    client.close();
   
     return NextResponse.json({ message: 'Recipe deleted successfully', result });
   }
